4012280339b1481f7de55c01828a29c77c31e2c8
SonaPrajapati <sona23f@gmail.com>
2025-06-29 01:21:28 +0530

    saving progress

--- controllers/user.go
+++ controllers/user.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/sonaprajapati/crud-mongodb/models"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -18,4 +21,41 @@ func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httpr
 	if !bson.IsObjectIdHex("id") {
 		w.WriteHeader(http.StatusNotFound)
 	}
+
+	oid := bson.ObjectIdHex(id)
+
+	u := models.User()
+
+	if err := uc.Session.DB("mongo-golang").C("user").FindId(oid).One(&u); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s/n", uj)
+}
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.Id = bson.NewObjectId()
+
+	uc.session.DB("mongo-golang").C("users").Insert(u)
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Println(w, "%s/n", uj)
 }
